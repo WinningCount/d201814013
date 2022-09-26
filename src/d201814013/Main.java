630b2339436695ee50afd9d8d6e35682e6f2ba9f
WinningCount <lw23570@gmail.com>
2022-09-26 15:37:16 +0900

    Main.java 수정#3

--- src/d201814013/Main.java
+++ src/d201814013/Main.java
@@ -1,13 +1,10 @@
 package d201814013;
 
 public class Main {
-	
-	static void say(String s) {
-		System.out.println(s);
-	}
 
 	public static void main(String[] args) {
-		String s = "hello world";
+		String s = "안녕하세요";
+		System.out.println(s);
 	}
 
 }
